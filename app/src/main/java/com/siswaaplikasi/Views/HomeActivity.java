69fdd6ea29f30617bbe9fd41ed391fad71cbc12f
Mudiono <Mudiono@atomic.id>
2020-03-28 17:41:48 +0700

    add home activity

---
+++ app/src/main/java/com/siswaaplikasi/Views/HomeActivity.java
@@ -0,0 +1,53 @@
+package com.siswaaplikasi.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.siswaaplikasi.Model.RombelModel;
+import com.siswaaplikasi.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnSiswa)
+    Button mBtnSiswa;
+    @BindView(R.id.btnRayon)
+    Button mBtnRayon;
+    @BindView(R.id.btnRembol)
+    Button mBtnRombel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        mBtnRayon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, RayonActivity.class);
+                startActivity(i);
+            }
+        });
+        mBtnRombel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, RombelActivity.class);
+                startActivity(i);
+            }
+        });
+        mBtnSiswa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
